3d286365b9994653d54c95f3a34a5a101b332afb
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-03-23 21:04:52 +0100

    Add sudo to commands

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 set -e
 
 INSTALL_DIR="usr/local/bin"
-mkdir -p "$INSTALL_DIR"
 SCRIPT_NAME="vencord"
 
 echo "[INFO] Installing $SCRIPT_NAME..."
-curl -fsSL https://raw.githubusercontent.com/lunarwanderer13/vencord-cli/refs/heads/main/bin/vencord -o "$INSTALL_DIR/$SCRIPT_NAME"
-chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
+sudo mkdir -p "$INSTALL_DIR"
+sudo curl -fsSL https://raw.githubusercontent.com/lunarwanderer13/vencord-cli/refs/heads/main/bin/vencord -o "$INSTALL_DIR/$SCRIPT_NAME"
+sudo chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
 echo "[INFO] Installed $SCRIPT_NAME to $INSTALL_DIR"
